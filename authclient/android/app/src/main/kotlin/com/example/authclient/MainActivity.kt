0fa444ca2cb8c93c34074af125a4302aedc6f343
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-19 20:24:57 +0530

    jwt added

---
+++ authclient/android/app/src/main/kotlin/com/example/authclient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authclient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ authclient/lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:authclient/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'login.dart';
+
+final storage = FlutterSecureStorage();
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "jwt");
+    if (jwt == null) return "";
+    return jwt;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Authentication Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+          future: jwtOrEmpty,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return CircularProgressIndicator();
+            if (snapshot.data != "") {
+              var str = snapshot.data;
+              var jwt = str.split(".");
+
+              if (jwt.length != 3) {
+                return LoginPage();
+              } else {
+                var payload = json.decode(
+                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
+                    .isAfter(DateTime.now())) {
+                  return HomePage(str, payload);
+                } else {
+                  return LoginPage();
+                }
+              }
+            } else {
+              return LoginPage();
+            }
+          }),
+    );
+  }
+}
