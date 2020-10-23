e84ec6ba9259f8cdcf48cdc171cd302d7c07ece6
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-23 20:42:04 +0530

    jwt added

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const verification =require('./verify');
 router.get('/',verification,(req, res) => {
-    res.json({ posts: { title: 'my first post', description: 'random data' } })
+    res.send(req.user);
 })
 
 
