a00cef80248b4866489d2b230c42e8f8f788d350
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-23 20:41:17 +0530

    jwt added

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app= express()
 const authRoute= require('./routes/auth')
 const mongoose= require('mongoose')
 const dotenv= require('dotenv')
+const postroute= require('./routes/post')
 
 dotenv.config();
 
 mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true},()=>console.log('Connected to DB'))
 app.use(express.json());
 app.use('/api/user',authRoute);
-
+app.use('/api/posts',postroute);
 app.listen(3000,()=>console.log('Server up and running'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/user')
 const bcrypt= require('bcryptjs')
+const jwt= require('jsonwebtoken')
 
-const Joi = require('@hapi/joi')
+const Joi = require('@hapi/joi');
+const { valid } = require('@hapi/joi');
 
 
 
@@ -18,8 +20,18 @@ const Lschema = Joi.object({
     password: Joi.string().min(6).required(),
 })
 
-router.post('/login', (req, res) => {
-    res.send('login')
+router.post('/login', async(req, res) => {
+    const { error } = Lschema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email doesnt exist')
+
+    const validpass= await bcrypt.compare(req.body.password,user.password);
+    if(!validpass) return res.status(400).send('Password Wrong')
+    
+    const token= jwt.sign({_id:user._id},process.env.TOKEN_SECRET)
+    res.header('auth-token',token).send(token);
+    res.send('Logged in');
 })
 
 router.post('/register', async (req, res) => {
